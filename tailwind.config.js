19873714e24232a2d3a7df38ca662ebbb81e9620
VBobell <vinicius.leite@gamersclub.com.br>
2021-10-07 18:06:23 -0300

    fix: change tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
 const colors = require('tailwindcss/colors');
 
 module.exports = {
-  purge: [],
+  purge: {
+    enabled: true,
+    content: ['./src/**/*.tsx'],
+  },
   presets: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
